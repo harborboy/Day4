109a70e4b65cd52d68474462df81bcb992c466f0
Troy Lima <troy468@yahoo.com>
2015-01-11 20:32:24 -0500

    Horserace

---
+++ horse_race.rb
@@ -0,0 +1,35 @@
+class Horses
+	def initialize(horses)
+end
+end
+
+
+
+
+horses = ['horse1','horse2','horse3','horse4']
+
+
+puts "Horse Race Game!"
+puts "Press R to start the race."
+
+ 
+ 	puts horses
+ 	
+ 	
+ 	input = gets.chomp
+ if input == "r"
+
+ 	5.times{|horse1|print"*"}
+ 	5.times{|horse2|print"*"}
+
+
+ 	puts horses
+
+
+
+end
+ 	 
+ 	
+
+
+ 
